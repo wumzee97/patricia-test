323d9c59dfe6702b6c1169a14c6e59ab122b4cf0
mac <adewumi.alabi@fundall.io>
2021-05-13 09:37:54 +0100

    Updates

--- pages/index.vue
+++ pages/index.vue
@@ -70,9 +70,9 @@ export default {
 
   background-color: #131313 !important;
   background-image: url('../static/assets/patricia icon.png');
-  border: 1px solid #000000 ;
+  border: 1px solid #000000 !important;
   box-sizing: border-box;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25) !important;
   border-radius: 20px !important;
 }
 p.card-pan {
