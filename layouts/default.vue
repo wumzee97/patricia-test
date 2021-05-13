14a0ddea4cdac31d2c93e4343c688746cc624e26
mac <adewumi.alabi@fundall.io>
2021-05-13 09:24:54 +0100

    First commit

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container fill-height>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }} Patricia Test - Alabi Adewumi</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      fixed: false,
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,109 @@
+<template>
+  <v-container>
+    <v-layout row wrap justify-center align-center>
+      <v-flex md6>
+        <v-card class="debit-card pa-9">
+          <v-row>
+            <v-col>
+              <img src="../static/assets/logo.svg" alt="" />
+            </v-col>
+            <v-col class="text-right pa-2">
+              <v-btn @click="visible = !visible" icon class="pa-0" small>
+                <v-icon size="30" color="#575A65">{{
+                  visible ? 'visibility' : 'visibility_off'
+                }}</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+          <v-row class="mt-16">
+            <v-col>
+              <p class="card-pan">{{ visible ? '9273' : '••••' }}</p>
+            </v-col>
+            <v-col>
+              <p class="card-pan">{{ visible ? '6372' : '••••' }}</p>
+            </v-col>
+            <v-col>
+              <p class="card-pan">{{ visible ? '7381' : '••••' }}</p>
+            </v-col>
+            <v-col>
+              <p class="card-pan">9784</p>
+            </v-col>
+          </v-row>
+          <v-row class="mt-16">
+            <v-col cols="7">
+              <p class="card-holder">
+                {{ visible ? 'ALABI ADEWUMI D.' : 'Cardholder Name' }}
+              </p>
+            </v-col>
+            <v-col class="text-right">
+              <div class="d-flex">
+                <p class="small mt-1">VALID<br />THRU</p>
+                <p class="date ml-3">10/12</p>
+              </div>
+            </v-col>
+            <v-col class="text-right pr-0">
+              <div class="d-flex text-right">
+                <p class="small mt-2">CVV</p>
+                <p class="date ml-3">{{ visible ? '672' : '•••' }}</p>
+              </div>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      visible: false,
+    }
+  },
+}
+</script>
+<style lang="scss" scoped>
+.debit-card {
+  width: 499px;
+  height: 305px;
+
+  background-color: #131313;
+  background-image: url('../static/assets/patricia icon.png');
+  border: 1px solid #000000;
+  box-sizing: border-box;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+}
+p.card-pan {
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  font-style: normal;
+  font-weight: normal;
+  font-size: 23px;
+  line-height: 29px;
+  letter-spacing: 0.08em;
+  color: #d3dce6;
+}
+p.card-holder {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 23px;
+  color: #d3dce6;
+}
+p.small {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 7px;
+  line-height: 8px;
+  color: #9da8b6;
+}
+p.date {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 27px;
+
+  color: #d3dce6;
+}
+</style>
